14d6e2519ac2a60d4bcdb514127f0bf76ca4b3ac
irSilviana <76592439+irSilviana@users.noreply.github.com>
2021-05-16 17:00:00 +0700

    Delete Task & Prop Drilling

--- src/App.js
+++ src/App.js
@@ -1,10 +1,38 @@
-import React from "react";
+import React, { useState } from "react";
 import Header from "./components/Header";
+import Tasks from "./components/Tasks";
 
 function App() {
+  const [tasks, setTasks] = useState([
+    {
+      id: 1,
+      text: "Doctors Appointment",
+      day: "Feb 5th at 2:30pm",
+      reminder: true,
+    },
+    {
+      id: 2,
+      text: "Meeting at School",
+      day: "Feb 6th at 1:30pm",
+      reminder: true,
+    },
+    {
+      id: 3,
+      text: "Food Shooping",
+      day: "Feb 10th at 4:30pm",
+      reminder: false,
+    },
+  ]);
+
+  // Delete Task
+  function deleteTask(id) {
+    setTasks(tasks.filter((task) => task.id !== id));
+  }
+
   return (
     <div className="container">
       <Header />
+      <Tasks tasks={tasks} onDelete={deleteTask} />
     </div>
   );
 }
