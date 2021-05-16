a95a856d5bc09769d7531728c26cd55e9e1d2380
irSilviana <76592439+irSilviana@users.noreply.github.com>
2021-05-17 02:38:27 +0700

    Update App.js
    Add task to server

--- src/App.js
+++ src/App.js
@@ -23,12 +23,23 @@ function App() {
   };
 
   // Add Task
-  function addTask(task) {
-    const id = Math.floor(Math.random() * 1000) + 1;
+  const addTask = async (task) => {
+    const res = await fetch(`http://localhost:5000/tasks`, {
+      method: "POST",
+      headers: {
+        "Content-type": "application/json",
+      },
+      body: JSON.stringify(task),
+    });
 
-    const newTask = { id, ...task };
-    setTasks([...tasks, newTask]);
-  }
+    const data = await res.json();
+    setTasks([...tasks, data]);
+
+    // const id = Math.floor(Math.random() * 1000) + 1;
+
+    // const newTask = { id, ...task };
+    // setTasks([...tasks, newTask]);
+  };
 
   // Delete Task
   async function deleteTask(id) {
